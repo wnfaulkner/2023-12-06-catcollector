c72ee2295dac6c37549b8191b7b06478cd9fe41c
Jim Clark <jim.clark@generalassemb.ly>
2022-10-25 06:49:36 -0700

    sync-5-finish-urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+# Baby step - Usually a Model is used
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def cats_index(request):
+  return render(request, 'cats/index.html', {
+    'cats': cats
+  })
