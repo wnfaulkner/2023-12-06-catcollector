9c81ec3c77b4889ca919f94a32d5531492c3af35
Jim Clark <jim.clark@generalassemb.ly>
2022-10-26 10:45:54 -0700

    sync-7-cat-model

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 
-# Baby step - Usually a Model is used
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
 
 # Create your views here.
 def home(request):
