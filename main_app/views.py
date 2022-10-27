2495a7a1dc8668c04fa3e2665a491333bbcd1c8a
Jim Clark <jim.clark@generalassemb.ly>
2022-10-27 13:40:19 -0700

    sync-12-class-views-finish

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Create your views here.
@@ -24,3 +24,11 @@ def cats_detail(request, cat_id):
 class CatCreate(CreateView):
   model = Cat
   fields = '__all__'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
