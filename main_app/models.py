14758774f729ec86c23e08e5401d6bd6857f01f5
Jim Clark <jim.clark@generalassemb.ly>
2022-10-31 15:07:04 -0700

    sync-17-finish-with-bonus

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -22,6 +23,9 @@ class Cat(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'cat_id': self.id})
+
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
