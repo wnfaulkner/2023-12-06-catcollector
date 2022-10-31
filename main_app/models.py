a71289ddb1706b602a8a12f4fd28cc92a5c9d92f
Jim Clark <jim.clark@generalassemb.ly>
2022-10-31 13:57:26 -0700

    sync-14-feeding-model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
 # Create your models here.
 class Cat(models.Model):
   name = models.CharField(max_length=100)
@@ -16,3 +22,20 @@ class Cat(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'cat_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Create a cat_id FK
+  cat = models.ForeignKey(
+    Cat,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
