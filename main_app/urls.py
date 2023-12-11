c8f4350b985591c2dbd1fcd8a8c1239f5f6a2b8f
wnf <william@fluxrme.com>
2023-12-11 15:59:17 -0600

    completes photo upload lesson

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,5 @@
+# URLS
+
 from django.urls import path
 from . import views
 
@@ -11,6 +13,7 @@ urlpatterns = [
   path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
   path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
   path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+  path('cats/<int:cat_id>/add_photo/', views.add_photo, name='add_photo'),
 
   path('cats/<int:cat_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
   path('cats/<int:cat_id>/disassoc_toy/<int:toy_id>/', views.disassoc_toy, name='disassoc_toy'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
+# VIEWS
+
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Cat, Toy
+from .models import Cat, Toy, Photo
 from .forms import FeedingForm
 
 # Create your views here.
@@ -57,6 +62,26 @@ def add_feeding(request, cat_id):
     new_feeding.cat_id = cat_id
     new_feeding.save()
   return redirect('detail', cat_id=cat_id)
+
+def add_photo(request, cat_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            # build the full url string
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, cat_id=cat_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('detail', cat_id=cat_id)
 
 def assoc_toy(request, cat_id, toy_id):
   # Note that you can pass a toy's id instead of the whole toy object
